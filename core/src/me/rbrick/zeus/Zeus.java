7b778f8840b4261035b1126d77bc6397c79feb49
rbrick <rbrickproductions123@gmail.com>
2014-10-27 22:24:49 -0500

    And so it begins....

---
+++ core/src/me/rbrick/zeus/Zeus.java
@@ -0,0 +1,12 @@
+package me.rbrick.zeus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ryan on 10/27/2014
+ * <p/>
+ * Project: ZeusV2
+ */
+public class Zeus extends JavaPlugin {
+    // Dummy class. lol
+}
